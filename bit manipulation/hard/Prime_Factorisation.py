396148a5f057e3193f32ed6a80aea767e9cbf8cc
Ishan Sharma <85005415+ishansharma25@users.noreply.github.com>
2024-06-06 22:29:57 +0530

    Update Prime_Factorisation.py

--- bit manipulation/hard/Prime_Factorisation.py
+++ bit manipulation/hard/Prime_Factorisation.py
@@ -1,21 +1,23 @@
-
 class Solution:
-    def sieve(self,x):
-        i=2
-        l=[]
-        while(i*i<=x):
-            if(x%i==0):
-                while(x%i==0):
-                    l.append(i)
-                    x=x//i
-            i=i+1
-        if x > 1:
-            l.append(x)
-        return l  
-        
+    def sieve(self):
+        a=10
     def findPrimeFactors(self, N):
+        def seve(x):
+            i=2
+            l=[]
+            while(i*i<=x):
+                if(x%i==0):
+                    while(x%i==0):
+                        l.append(i)
+                        x=x//i
+                i=i+1
+                
+            if x > 1:
+                l.append(x)
+            return l
         results = []
-        for i in range(N):
-            g=self.sieve(N[i])
-            results.append(g)
-        return results
+        for i in range(N+1):
+            g=seve(i)
+            #results.append(g)
+        return g
+        

fffc519ff645f24484bb0ad773a3bc28f54f43b0
Ishan Sharma <85005415+ishansharma25@users.noreply.github.com>
2024-06-15 10:09:28 +0530

    Update stack_using_linkedlist.py

--- stack/stack_using_linkedlist.py
+++ stack/stack_using_linkedlist.py
@@ -27,3 +27,31 @@ class MyStack:
         self.Stack=self.Stack.next
         return data
         # Add code here
+'''
+void MyStack ::push(int x) 
+{
+    StackNode *node=(StackNode*)malloc(sizeof(StackNode));
+    node->data=x;
+    node->next=top;
+    top=node;
+    
+    // Your Code
+}
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop() 
+{
+    if(top==NULL)
+    {
+        return -1;
+    }
+    StackNode *p;
+    int x;
+    p=top;
+    top=top->next;
+    x=p->data;
+    free(p);
+    return x;
+    // Your Code
+}
+'''
